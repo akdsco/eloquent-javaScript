1083ed0032bce782007bba820dcbcb5fb64f058d
akdsco <arkadiusz@ostrowski.co>
2019-07-25 10:28:24 +0100

    add flatten array function

---
+++ 5 - Higher Order Functions/flattening.js
@@ -0,0 +1,24 @@
+/* Flattening (Using reduce and concat)
+ *
+ * Use the reduce method in combination with the concat method to “flatten” an array of arrays 
+ * into a single array that has all the elements of the original arrays.
+*/
+
+let arrays = [[1, 2, 3], [4, 5], [6], [7]];
+
+function flatArray(arr) {
+  return arr.reduce((flat, toFlatten) => flat.concat(toFlatten)) 
+}
+
+console.log(flatArray(arrays))
+
+// -> [1, 2, 3, 4, 5, 6, 7]
+
+// Also found online a solution that included flattening further embeded arrays.
+// It uses recursive approach to flatten inner arrays.
+
+function flatten(arr) {
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+  }, []);
+}
